e741422541d8a66be369d4c853fad9d77c79d5ea
Alan McGovern <alan.mcgovern@gmail.com>
2021-12-02 21:49:01 +0000

    day2

---
+++ src/day2/Program.cs
@@ -0,0 +1,8 @@
+﻿var results = File.ReadAllLines("input.txt")
+    .Select(t => t.Split(' '))
+    .Select(t => (t[0], int.Parse(t[1])))
+    .GroupBy(t => t.Item1, v => v.Item2)
+    .ToDictionary(group => group.Key, group => group.Sum());
+var depth = results["down"] - results["up"];
+var forward = results["forward"];
+Console.WriteLine($"Depth: {depth}. Forward: {forward}. Product: {forward * depth}");
