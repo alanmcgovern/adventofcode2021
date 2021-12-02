fd2ab01e56036f5fea6666e2b6becf68559ec1c1
Alan McGovern <alan.mcgovern@gmail.com>
2021-12-02 21:18:17 +0000

    Solve the first problem
    Do it functionally, with LINQ

---
+++ src/day1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+var sum = File.ReadAllLines("input.txt")
+    .Select(int.Parse)
+    .Pairwise()
+    .Select(pair => pair.Left < pair.Right ? 1 : 0)
+    .Sum();
+Console.WriteLine($"Answer: {sum}");
+
+public static class Extensions
+{ 
+    public static IEnumerable<(T? Left, T? Right)> Pairwise<T> (this IEnumerable<T?> input)
+    {
+        bool hasFirst = false;
+        T? first;
+        T? second = default;
+
+        foreach (var v in input)
+        {
+            first = second;
+            second = v;
+            if (hasFirst)
+                yield return (first, second);
+            hasFirst = true;
+        }
+    }
+}
