34d7d355df3604073ab37b3b27252c7657f64ac8
Alan McGovern <alan.mcgovern@gmail.com>
2021-12-02 21:28:54 +0000

    Do this without code duplication

--- src/day1/Program.cs
+++ src/day1/Program.cs
@@ -1,26 +1,32 @@
 ﻿
 var sum = File.ReadAllLines("input.txt")
     .Select(int.Parse)
-    .Pairwise()
-    .Select(pair => pair.Left < pair.Right ? 1 : 0)
-    .Sum();
-Console.WriteLine($"Answer: {sum}");
+    .Pairwise(2)
+    .Sum(tuple => tuple[0] < tuple[1] ? 1 : 0);
+Console.WriteLine($"Answer Challenge 1: {sum}");
+
+sum = File.ReadAllLines("input.txt")
+    .Select(int.Parse)
+    .Pairwise(3)
+    .Select(triplet => triplet.Sum ())
+    .Pairwise(2)
+    .Sum(pair => pair[0] < pair[1] ? 1 : 0);
+Console.WriteLine($"Answer Challenge 2: {sum}");
 
 public static class Extensions
-{ 
-    public static IEnumerable<(T? Left, T? Right)> Pairwise<T> (this IEnumerable<T?> input)
+{
+    public static IEnumerable<T?[]> Pairwise<T>(this IEnumerable<T?> input, int n)
     {
-        bool hasFirst = false;
-        T? first;
-        T? second = default;
+        var values = new List<T?>(n);
 
         foreach (var v in input)
         {
-            first = second;
-            second = v;
-            if (hasFirst)
-                yield return (first, second);
-            hasFirst = true;
+            values.Add(v);
+            if (values.Count == n)
+            {
+                yield return values.ToArray();
+                values.RemoveAt(0);
+            }
         }
     }
 }
