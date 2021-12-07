1287f4963fad89f116077cf94ba7a7de9227bc39
alan.mcgovern <alan.mcgovern@gmail.com>
2021-12-07 22:23:53 +0000

    Circular array to calculate fish!

--- 2021/csharp/day6/Program.cs
+++ 2021/csharp/day6/Program.cs
@@ -5,16 +5,12 @@ var fishes = File.ReadAllText("input.txt")
     .Select(int.Parse)
     .GroupBy(t => t);
 
-foreach (var days in new int[] { 80, 256 }) {
-    var fishByAge = new List<long>(new long[9]);
+foreach (var days in new int[] { 18, 80, 256 }) {
+    var fishByAge = new long[9];
     foreach (var age in fishes)
         fishByAge[age.Key] = age.Count();
 
     for (int i = 0; i < days; i++)
-    {
-        fishByAge[7] += fishByAge[0];
-        fishByAge.Add(fishByAge[0]);
-        fishByAge.RemoveAt(0);
-    }
+        fishByAge[(i + 7) % 9] += fishByAge[(i % 9)];
     Console.WriteLine($"Total fish after {days} days: {fishByAge.Sum()}");
 }
