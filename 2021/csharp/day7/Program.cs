5d53b7c78276c3b97dcebfee1cde8fcbba168497
alan.mcgovern <alan.mcgovern@gmail.com>
2021-12-07 21:46:53 +0000

    Day 7 - brute force it?

---
+++ 2021/csharp/day7/Program.cs
@@ -0,0 +1,21 @@
+﻿var positions = File.ReadAllText("input.txt")
+    .Split(',')
+    .Select(t => int.Parse (t))
+    .ToArray();
+
+var lowestPosition = positions.Min();
+var highestPosition = positions.Max();
+
+int bestPosition = -100;
+var lowestCost = int.MaxValue;
+for (int i = lowestPosition; i <= highestPosition; i++)
+{
+    var cost = positions.Select(position => Math.Abs(position - i)).Sum();
+    if (cost < lowestCost)
+    {
+        lowestCost = cost;
+        bestPosition = i;
+    }
+}
+
+Console.WriteLine($"Best position {bestPosition}. Fuel cost: {lowestCost}");
