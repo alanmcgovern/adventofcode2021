18550276c09d545b57cd5cb05cff31dd071932ef
Alan McGovern <alan.mcgovern@gmail.com>
2021-12-10 22:19:11 +0000

    AAAaand q2. I made this one functional rather than iterative

--- 2021/csharp/day10/Program.cs
+++ 2021/csharp/day10/Program.cs
@@ -10,31 +10,57 @@ var segments = new List<Segment>
     new Segment ('<', '>', 25137),
 };
 
-var value = 0;
+var incompleteLines = new List<Stack<char>>();
+
+long value = 0;
 foreach (var line in input)
 {
     var stack = new Stack<char>();
+    bool corrupt = false;
     foreach (var c in line)
     {
-        if (segments.Any (t => t.Opening == c))
-            stack.Push(c);
-        if (segments.Any(t => t.Closing == c))
+        if (segments.Any(t => t.Opening == c))
         {
-            if (stack.Count == 0)
-                break;
-            else if (segments.Single (t => t.Closing == c).Opening != stack.Pop())
-                value += segments.Single (segment => segment.Closing == c).Points;
+            stack.Push(c);
+        }
+        else if (segments.Any(t => t.Closing == c))
+        {
+            if (segments.Single(t => t.Closing == c).Opening != stack.Pop())
+            {
+                corrupt = true;
+                value += segments.Single(segment => segment.Closing == c).Points;
+            }
         }
     }
+    if (!corrupt)
+        incompleteLines.Add(stack);
 }
 Console.WriteLine($"Parser points: {value}");
 
+
+// Question 2
+segments = new List<Segment>
+{
+    new Segment ('(', ')', 1),
+    new Segment ('[', ']', 2),
+    new Segment ('{', '}', 3),
+    new Segment ('<', '>', 4),
+};
+
+var autocompletePoints = incompleteLines
+    .Select(line =>
+        line.Select(opener => segments.Single(t => t.Opening == opener).Points)
+       .Aggregate((a, b) => a * 5 + b)
+    ).OrderBy(t => t)
+    .ToArray();
+
+Console.WriteLine($"Autocomplete points: {autocompletePoints[autocompletePoints.Length / 2]}");
 
 readonly struct Segment
 {
     public readonly char Opening;
     public readonly char Closing;
-    public readonly int Points;
+    public readonly long Points;
 
     public Segment(char opening, char closing, int points)
         => (Opening, Closing, Points) = (opening, closing, points);
