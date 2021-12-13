7ccb270b70cf44bcefedf11fe6cdbbc322fe08e3
Alan McGovern <alan.mcgovern@gmail.com>
2021-12-13 22:42:51 +0000

    day 13 q1
    This is a set of subtractions and then a de-dupe at the end

---
+++ 2021/csharp/day13/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+
+using Instructions = System.ReadOnlyMemory<(Axis axis, int position)>;
+
+var input = File.ReadAllLines("input.txt");
+
+
+ReadOnlyMemory<Point> points = input
+    .TakeWhile(t => !string.IsNullOrEmpty(t))
+    .Select(t => t.Split(','))
+    .Select(t => new Point(int.Parse(t[0]), int.Parse(t[1])))
+    .ToArray();
+
+Instructions instructions = input.SkipWhile(t => !string.IsNullOrEmpty(t)).Skip (1)
+    .Select(t => t.Split(' ').Last())
+    .Select(t => t.Split('='))
+    .Select<string[], (Axis axis, int position)>(t => (Enum.Parse<Axis>(t[0], true), int.Parse(t[1])))
+    .ToArray();
+
+// Q1
+var result = points;
+foreach (var instruction in instructions.Span.ToArray ().Take (1))
+    result = Fold(instruction.axis, instruction.position, result);
+Console.WriteLine($"Total visible: {new HashSet<Point>(result.Span.ToArray()).Count}");
+
+
+// Q2
+
+
+static ReadOnlyMemory<Point> Fold (Axis axis, int position, ReadOnlyMemory<Point> points)
+{
+    var result = new List<Point> (points.Length);
+    foreach (var point in points.Span)
+    {
+        switch (axis)
+        {
+            case Axis.X:
+                if (point.X == position)
+                    continue;
+                if (point.X < position)
+                    result.Add(point);
+                else
+                    result.Add(new Point(point.X - 2 * (point.X - position), point.Y));
+                break;
+
+            case Axis.Y:
+                if (point.Y == position)
+                    continue;
+                if (point.Y < position)
+                    result.Add(point);
+                else
+                    result.Add(new Point(point.X, point.Y - 2 * (point.Y - position)));
+                break;
+        }
+    }
+    return result.ToArray();
+}
+
+enum Axis
+{
+    X,
+    Y
+}
