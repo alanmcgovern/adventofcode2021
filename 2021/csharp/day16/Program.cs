b360be2805ece95bf436a87b7216e53c141c9fce
Alan McGovern <alan.mcgovern@gmail.com>
2021-12-20 13:31:52 +0000

    Parse it into a struture because it's a little easier

--- 2021/csharp/day16/Program.cs
+++ 2021/csharp/day16/Program.cs
@@ -1,107 +1,140 @@
-﻿bool sample = false;
+﻿using System.Collections;
 
-ReadOnlyMemory<char> binary = File.ReadAllLines(sample ? "sample.txt" : "input.txt")
+// Convert to 'binary' 1s and 0s 
+ReadOnlyMemory<byte> binary = File.ReadAllLines("input.txt")
     .SelectMany(line => line.ToArray())
     .SelectMany(c => Convert.ToString(Convert.ToInt32($"{c}", 16), 2).PadLeft(4, '0'))
+    .Select (c => (byte)(c - '0'))
     .ToArray()
     .AsMemory();
 
-Console.WriteLine($"Q1: {Parse(binary).Sum(t => t.Version)}");
+var packet = Packet.Parse(binary);
+Console.WriteLine($"Q1: {packet.Sum(t => t.Version)}");
+Console.WriteLine($"Q2: {packet.Compute()}");
 
-
-static IEnumerable<Header> Parse(ReadOnlyMemory<char> memory)
+struct Packet : IEnumerable<Packet>
 {
-    while (memory.Length > 0)
-    {
-        var packet = new Header(memory.Slice(0, 6));
-        memory = memory.Slice(6);
+    public static Packet Parse(ReadOnlyMemory<byte> memory)
+        => Parse(ref memory);
 
-        if (packet.PacketType == 4)
-        {
-            memory = memory.Slice(LiteralValuePacket.Parse(memory).Length);
-        }
+    public static Packet Parse(ref ReadOnlyMemory<byte> memory)
+    {
+        var packetMemory = memory.Slice(6);
+        if ((Type)memory.Slice(3, 3).ToLong() == Type.LiteralValue)
+            _ = ParseLiteralValue(ref packetMemory);
+        else if (packetMemory.Span[0] == 0)
+            packetMemory = packetMemory.Slice(16);
         else
+            packetMemory = packetMemory.Slice(12);
+
+        var packet = new Packet(memory.Slice(0, memory.Length - packetMemory.Length));
+        memory = memory.Slice(packet.Memory.Length);
+
+        if (packet.Value.lengthInBits > 0)
         {
-            if (memory.Span[0] == '0')
-                memory = memory.Slice(16);
-            else
-                memory = memory.Slice(12);
+            var remaining = memory.Length - packet.Value.lengthInBits.Value;
+            while (remaining != memory.Length)
+                packet.Operands.Add(Parse(ref memory));
         }
-        yield return packet;
+        else if (packet.Value.lengthInMessages > 0)
+        {
+            while (packet.Operands.Count != packet.Value.lengthInMessages.GetValueOrDefault(0))
+                packet.Operands.Add(Parse(ref memory));
+        }
+        return packet;
     }
-}
 
+    static long ParseLiteralValue(ReadOnlyMemory<byte> memory)
+        => ParseLiteralValue(ref memory);
 
-class Header
-{
-    ReadOnlyMemory<char> Memory { get; }
-    protected ReadOnlySpan<char> Span => Memory.Span;
-
-    public int Length => Memory.Length;
-
-    public int Version => Span.Slice(0, 3).ToInt ();
-    public int PacketType => Span.Slice(3, 3).ToInt();
-
-    public Header(ReadOnlyMemory<char> memory)
-        => (Memory) = (memory);
-}
-
-class Packet
-{
-    protected ReadOnlyMemory<char> Memory { get; }
-    protected ReadOnlySpan<char> Span => Memory.Span;
-
-    public int Length => Memory.Length;
-    public Packet(ReadOnlyMemory<char> memory)
-        => Memory = memory;
-}
-
-class OperatorPacket
-{
-    public List<Packet> Packets { get; } = new List<Packet>();
-}
-
-class LiteralValuePacket : Packet
-{
-    public static LiteralValuePacket Parse (ReadOnlyMemory<char> memory)
+    static long ParseLiteralValue(ref ReadOnlyMemory<byte> memory)
     {
-        var start = memory;
-        for (int i = 0; i < start.Length; i += 5)
-            if (start.Span[i] == '0')
-                return new LiteralValuePacket (start.Slice(0, i + 5));
-        throw new InvalidDataException();
-
+        long value = 0;
+        bool shouldExit = false;
+        while (memory.Length > 0 && !shouldExit)
+        {
+            value = (value << 4) + memory.Span.Slice(1, 4).ToLong();
+            shouldExit = memory.Span[0] == 0;
+            memory = memory.Slice(5);
+        }
+        return value;
     }
 
-    public LiteralValuePacket(ReadOnlyMemory<char> memory)
-        : base(memory)
+    public IEnumerator<Packet> GetEnumerator()
     {
+        yield return this;
 
+        foreach (var child in Operands)
+            foreach (var message in child)
+                yield return message;
     }
 
-    public int Value
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+
+    public ReadOnlyMemory<byte> Memory { get; }
+
+    public long Version => Memory.Slice(0, 3).ToLong();
+    public Type PacketType => (Type)Memory.Slice(3, 3).ToLong();
+
+    public (long? lengthInBits, long? lengthInMessages, long? literalValue) Value
     {
         get
         {
-            int value = 0;
-            var valueSpan = Span;
-            while (valueSpan.Length > 0)
-            {
-                value = (value << 4) + valueSpan.Slice(1, 4).ToInt();
-                valueSpan = valueSpan.Slice(5);
-            }
-            return value;
+            var dataStart = Memory.Slice(6);
+            if (PacketType == Type.LiteralValue)
+                return (default, default, ParseLiteralValue(dataStart));
+            else if (dataStart.Span[0] == 0)
+                return (dataStart.Slice(1).ToLong(), default, default);
+            else
+                return (default, dataStart.Slice(1).ToLong(), default);
         }
     }
+
+    public List<Packet> Operands { get; } = new List<Packet>();
+
+    Packet(ReadOnlyMemory<byte> memory)
+    {
+        Memory = memory;
+    }
+
+    public long Compute()
+        => PacketType switch
+        {
+            Type.Sum => Operands.Sum(t => t.Compute()),
+            Type.Product => Operands.Select(t => t.Compute()).Aggregate((left, right) => left * right),
+            Type.Min => Operands.Select(t => t.Compute()).Min(),
+            Type.Max => Operands.Select(t => t.Compute()).Max(),
+            Type.LiteralValue => Value.literalValue!.Value,
+            Type.GreaterThan => Operands[0].Compute() > Operands[1].Compute() ? 1 : 0,
+            Type.LessThan => Operands[0].Compute() < Operands[1].Compute() ? 1 : 0,
+            Type.Equal => Operands[0].Compute() == Operands[1].Compute() ? 1 : 0,
+            _ => throw new NotSupportedException()
+        };
 }
 
 static class Extensions
 {
-    public static int ToInt(this ReadOnlySpan<char> span)
+    public static long ToLong(this ReadOnlyMemory<byte> memory)
+        => memory.Span.ToLong();
+
+    public static long ToLong(this ReadOnlySpan<byte> span)
     {
-        int result = 0;
+        long result = 0;
         for (int i = 0; i < span.Length; i++)
-            result |= (span[i] == '1' ? 1 : 0) << (span.Length - i - 1);
+            result |= (span[i] == 1 ? 1L : 0) << (span.Length - i - 1);
         return result;
     }
+}
+
+enum Type
+{
+    Sum = 0,
+    Product = 1,
+    Min = 2,
+    Max = 3,
+    LiteralValue = 4,
+    GreaterThan = 5,
+    LessThan = 6,
+    Equal = 7
 }
