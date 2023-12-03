9ba27a13579ed6f7b44a5919906f705f1fa6a2dc
alan.mcgovern <alan.mcgovern@gmail.com>
2023-12-03 14:22:41 +0000

    Huh, i was convinced 'eightwo' converted as '8wo'.
    And if that were true then order mattered.
    
    Since it's actually '82', then we just need to preserve the first/last
    letters of each word when we do the replacement.
    fffffffffff

--- 2023/csharp/day01/Program.cs
+++ 2023/csharp/day01/Program.cs
@@ -1,7 +1,32 @@
-﻿var numbers = File.ReadAllLines("input.txt")
-    .Select(t => t.Where(digit => digit >= '0' && digit <= '9').ToArray ())
-    .Select (t => $"{t.First ()}{t.Last ()}")
-    .Select (int.Parse)
-    .ToArray();
+﻿var lines = File.ReadAllLines("input.txt");
 
-Console.WriteLine($"Q1: {numbers.Sum ()}");
+IList<(string word, string number)> replacements = new[]
+{
+    ( "zero",   "z--0--o" )s,
+    ( "one",    "o--1--e" ),
+    ( "two",    "t--2--o" ),
+    ( "three",  "t--3--e" ),
+    ( "four",   "f--4--r" ),
+    ( "five",   "f--5--e" ),
+    ( "six",    "s--6--x" ),
+    ( "seven",  "s--7--n" ),
+    ( "eight",  "e--8--t" ),
+    ( "nine",   "n--9--e" ),
+}.AsReadOnly();
+
+static int CalculateCalibrationValue(IEnumerable<string> input, Func<string, string> filter)
+    => input
+    .Select(filter)
+    .Select(t => t.Where(char.IsDigit).ToArray())
+    .Select(t => $"{t.First()}{t.Last()}")
+    .Select(int.Parse)
+    .Sum();
+
+
+Console.WriteLine($"Q1: {CalculateCalibrationValue(lines, t => t)}");
+Console.WriteLine($"Q2: {CalculateCalibrationValue(lines, line =>
+{
+    foreach (var v in replacements)
+        line = line.Replace(v.word, v.number);
+    return line;
+})}");
