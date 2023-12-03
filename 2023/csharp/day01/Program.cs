a1df99d9874213f76b5ca2dfbd7a2ad749ad0db9
alan.mcgovern <alan.mcgovern@gmail.com>
2023-12-03 13:29:09 +0000

    2023 - q1 p1

---
+++ 2023/csharp/day01/Program.cs
@@ -0,0 +1,7 @@
+﻿var numbers = File.ReadAllLines("input.txt")
+    .Select(t => t.Where(digit => digit >= '0' && digit <= '9').ToArray ())
+    .Select (t => $"{t.First ()}{t.Last ()}")
+    .Select (int.Parse)
+    .ToArray();
+
+Console.WriteLine($"Q1: {numbers.Sum ()}");
