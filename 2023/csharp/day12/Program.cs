4977c8a46edb597faa8941f19c6d959d09219a0a
alan.mcgovern <alan.mcgovern@gmail.com>
2023-12-18 21:58:23 +0000

    day12

---
+++ 2023/csharp/day12/Program.cs
@@ -0,0 +1,32 @@
+﻿var records = File.ReadAllLines("input.txt").Select(ParseRecord).ToArray ();
+
+Console.WriteLine($"Q1: {records.Select(CountVariations).Sum()}");
+Console.Write(records);
+
+Record ParseRecord(string line)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    return new Record(
+        parts[0].Select(t => (SpringState)t).ToArray(),
+        parts[1].Split (',', StringSplitOptions.RemoveEmptyEntries).Select(t => int.Parse(t.ToString ())).ToArray()
+    );
+}
+
+static long CountVariations(Record record)
+{
+    var potential = record.States.Span;
+    return 0;
+}
+
+class Record(ReadOnlyMemory<SpringState> states, ReadOnlyMemory<int> parityCounts)
+{
+    public ReadOnlyMemory<SpringState> States { get; } = states;
+    ReadOnlyMemory<int> ParityCounts { get; } = parityCounts;
+}
+
+enum SpringState
+{
+    operational = '.',
+    damaged = '#',
+    unknown = '?'
+}
