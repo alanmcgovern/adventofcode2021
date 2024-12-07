1eb4b99956eee7c30c1e330f426eea831dad6179
Alan McGovern <alan.mcgovern@gmail.com>
2024-12-07 23:32:32 +0000

    d7p1

---
+++ 2024/Day07/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Day07
+{
+    class Program
+    {
+        enum Operator
+        {
+            Mul,
+            Add,
+            Pipe
+        }
+
+        static void Main (string[] args)
+        {
+            (long target, ReadOnlyMemory<long> operands)[] input = File.ReadAllLines ("input.txt").Select (line => {
+                var target = long.Parse (line.Substring (0, line.IndexOf (':')));
+                var operands = line.Substring (line.IndexOf (':') + 1).Split (' ', StringSplitOptions.RemoveEmptyEntries).Select (long.Parse).ToArray ();
+                return (target, (ReadOnlyMemory<long>) operands);
+            }).ToArray ();
+
+            Console.WriteLine ($"Q1: {input.Where (IsValid).Sum (t => t.target)}");
+        }
+
+        static bool IsValid ((long target, ReadOnlyMemory<long> operands) equation)
+            => Compute(equation.operands.Span[0],equation.operands.Slice(1)).Any (t => t == equation.target);
+
+        static IEnumerable<long> Compute(long val, ReadOnlyMemory<long> remainder)
+        {
+            if (remainder.Length == 0) {
+                yield return val;
+                yield break;
+            }
+
+            foreach(var op in allowedOperations) {
+                var nextVal = op switch {
+                    Operator.Mul => val * remainder.Span[0],
+                    Operator.Add => val + remainder.Span[0],
+                    _ => throw new InvalidOperationException ()
+                };
+                foreach (var v in Compute (nextVal, remainder.Slice (1)))
+                    yield return v;
+            }
+        }
+    }
+}
