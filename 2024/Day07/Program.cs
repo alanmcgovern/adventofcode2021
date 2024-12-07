c483dba01d44f4f0954ebb50b9881e2cedc362d2
Alan McGovern <alan.mcgovern@gmail.com>
2024-12-07 23:48:46 +0000

    concating strings was a bit ick
    Use integer math for the next power of 10.
    Ain't nobody got time for floating point inaccuracies
    with Math.Log10/Math.Ceil

--- 2024/Day07/Program.cs
+++ 2024/Day07/Program.cs
@@ -24,23 +24,34 @@
         static bool IsValid ((long target, ReadOnlyMemory<long> operands) equation, Operator[] allowedOperators)
             => Compute (equation.operands.Span[0], equation.operands[1..], allowedOperators).Any (t => t == equation.target);
 
-        static IEnumerable<long> Compute(long val, ReadOnlyMemory<long> remainder, Operator[] allowedOperators)
+        static IEnumerable<long> Compute (long val, ReadOnlyMemory<long> remainder, Operator[] allowedOperators)
         {
             if (remainder.Length == 0) {
                 yield return val;
                 yield break;
             }
 
-            foreach(var op in allowedOperators) {
+            foreach (var op in allowedOperators) {
                 var nextVal = op switch {
                     Operator.Mul => val * remainder.Span[0],
                     Operator.Add => val + remainder.Span[0],
-                    Operator.Pipe => long.Parse (val.ToString () + remainder.Span[0].ToString ()), // ain't nobody got time for 
+                    Operator.Pipe => val * NextPowerOf10 (remainder.Span[0]) + remainder.Span[0],
                     _ => throw new InvalidOperationException ()
                 };
                 foreach (var v in Compute (nextVal, remainder[1..], allowedOperators))
                     yield return v;
             }
+        }
+
+        public static long NextPowerOf10 (long n)
+        {
+            if (n < 1)
+                throw new NotSupportedException ();
+
+            long pow = 1;
+            while (pow <= n)
+                pow *= 10;
+            return pow;
         }
     }
 }
