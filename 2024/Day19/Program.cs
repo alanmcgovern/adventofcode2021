176dc7a7113f8a5d261bd13cd8a14be0c3bd3e70
Alan McGovern <alan.mcgovern@gmail.com>
2024-12-20 02:04:27 +0000

    d19p2
    I spent quite a while getting really confused as to how 'b' wasn't in
    the count dictionary even though i could see it there in the debugger.
    
    Then i realised i forgot to pass in the 'MemoryComparer' to the new
    dicts ctor. Whoops.

--- 2024/Day19/Program.cs
+++ 2024/Day19/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 
@@ -12,46 +13,34 @@ namespace Day19
             var towels = lines.First ().Split (", ").Select (t => t.AsMemory ()).ToArray ();
             var patterns = lines.Skip (2).ToArray ();
 
-            Console.WriteLine ($"Q1 {patterns.Count (t => {
-                return CanMake (t.AsMemory (), towels);
-            })}");
+            var madePatterns = patterns.Select (t => CanMakeRecursive (t.AsMemory (), towels)).ToArray ();
+            Console.WriteLine ($"Q1 {madePatterns.Where (t => t.canMake).Count ()}");
+            Console.WriteLine ($"Q2 {madePatterns.Sum (t => t.count)}");
         }
-
-        static bool CanMake (ReadOnlyMemory<char> pattern, ReadOnlyMemory<char>[] towels)
-        {
-            var uniqueCombos = towels.SelectMany (t => t.ToArray ()).ToHashSet ();
-            foreach (var letter in pattern.Span)
-                if (!uniqueCombos.Contains (letter))
-                    return false;
-
-            return CanMakeRecursive (pattern, pattern, towels);
-        }
-
 
         static readonly Dictionary<ReadOnlyMemory<char>, bool> cache = new Dictionary<ReadOnlyMemory<char>, bool> (new MemoryComparer ());
-        static readonly Dictionary<ReadOnlyMemory<char>, int> counts = new Dictionary<ReadOnlyMemory<char>, int> ();
-        static bool CanMakeRecursive (ReadOnlyMemory<char> fullPattern, ReadOnlyMemory<char> remainingPattern, ReadOnlyMemory<char>[] towels)
+        static readonly Dictionary<ReadOnlyMemory<char>, long> counts = new Dictionary<ReadOnlyMemory<char>, long> (new MemoryComparer ());
+        static (bool canMake, long count) CanMakeRecursive (ReadOnlyMemory<char> remainingPattern, ReadOnlyMemory<char>[] towels)
         {
-            if (remainingPattern.Length == 0) {
-                if (!counts.TryGetValue (fullPattern, out int count))
-                    count = 0;
-                counts[fullPattern] = count + 1;
-                return true;
-            }
-
             if (cache.TryGetValue (remainingPattern, out var cached))
-                return cached;
+                return (cached, counts[remainingPattern]);
 
-            for (int i = 0; i < towels.Length; i ++) {
+            if (remainingPattern.Length == 0)
+                return (true, 1);
+
+            for (int i = 0; i < towels.Length; i++) {
                 if (remainingPattern.Span.StartsWith (towels[i].Span)) {
-                    if (CanMakeRecursive (fullPattern, remainingPattern.Slice (towels[i].Span.Length), towels)) {
-                        cache.Add (remainingPattern, true);
-                        return true;
-                    }
+                    var (canMake, count) = CanMakeRecursive (remainingPattern.Slice (towels[i].Span.Length), towels);
+                    cache[remainingPattern] = canMake;
+                    counts[remainingPattern] = counts.GetValueOrDefault (remainingPattern, 0) + count;
                 }
             }
-            cache.Add (remainingPattern, false);
-            return false;
+
+            if (!counts.TryGetValue (remainingPattern, out var val)) {
+                cache[remainingPattern] = false;
+                counts[remainingPattern] = 0;
+            }
+            return (cache[remainingPattern], counts[remainingPattern]);
         }
 
         class MemoryComparer : IEqualityComparer<ReadOnlyMemory<char>>
@@ -60,7 +49,7 @@ namespace Day19
                 => x.Span.SequenceEqual (y.Span);
 
             public int GetHashCode ([DisallowNull] ReadOnlyMemory<char> obj)
-                => string.GetHashCode (obj.Span);
+                => string.GetHashCode (obj.Span, StringComparison.Ordinal);
         }
     }
 }
