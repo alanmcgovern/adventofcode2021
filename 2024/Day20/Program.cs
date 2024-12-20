fe3b048f0980429e2b0e71a0ce0ee56da19a5048
Alan McGovern <alan.mcgovern@gmail.com>
2024-12-20 20:38:53 +0000

    d20p2
    I spent a while debugging and then realised i was treating the cheat as
    teleport, not a 'drive through the wall' type thing.
    
    Tweaked the calculation to take into account the manhatten distance
    between the two points when calculating the saving. If we cheat our
    way to a node 100 in front of us, but it takes 10 moves to get there,
    the saving is only 90 moves.

--- 2024/Day20/Program.cs
+++ 2024/Day20/Program.cs
@@ -1,92 +1,87 @@
-﻿using System.Drawing;
+﻿using System.Collections.Frozen;
 using System.Numerics;
 
 namespace Day20
 {
+    static class Extensions
+    {
+        // We want a minimum saving of 100 picoseconds, so we should only make pairwise combinations of nodes at least 100 apart.
+        public static IEnumerable<((T item, int itemIndex) left, (T item, int itemIndex) right)> Pairwise<T> (this IList<T> list, int minimumOffset)
+        {
+            for (int i = 0; i < list.Count; i++) {
+                for (int j = i + minimumOffset; j < list.Count; j++)
+                    yield return ((list[i], i), (list[j], j));
+            }
+        }
+    }
+
     class Program
     {
         readonly record struct Position (int X, int Y) : IAdditionOperators<Position, Position, Position>
         {
+            public static readonly Position Left = new (-1, 0);
+            public static readonly Position Right = new (1, 0);
+            public static readonly Position Up = new (0, -1);
+            public static readonly Position Down = new (0, 1);
+
+            public static readonly FrozenSet<Position> AllDirections = new HashSet<Position> ([Left, Right, Up, Down]).ToFrozenSet ();
+
             public static Position operator + (Position left, Position right)
-                => new Position (left.X + right.X, left.Y + right.Y);
-        }
+                => new (left.X + right.X, left.Y + right.Y);
 
-        static readonly Position Left = new Position (-1, 0);
-        static readonly Position Right = new Position (1, 0);
-        static readonly Position Up = new Position (0, -1);
-        static readonly Position Down = new Position (0, 1);
-
-        enum Area
-        {
-            ThinWall,
-            Wall,
-            Track,
+            public int ManhattenDistance (Position other)
+                => Math.Abs (other.X - X) + Math.Abs (other.Y - Y);
         }
 
         static void Main (string[] args)
         {
             int y = 0;
             Position start = default, end = default;
-            var track = new Dictionary<Position, Area> ();
+            var trackPoints = new HashSet<Position> ();
             foreach (var line in File.ReadLines ("input.txt")) {
                 for (int x = 0; x < line.Length; x++) {
                     if (line[x] == 'S')
                         start = new Position (x, y);
                     else if (line[x] == 'E')
                         end = new Position (x, y);
-                    track.Add (new Position (x, y), line[x] == '#' ? Area.Wall : Area.Track);
+                    if (line[x] != '#')
+                        trackPoints.Add (new Position (x, y));
                 }
                 y++;
             }
 
-            // Pattern match for skippable walls.
-            //
-            //      track -> wall -> track
-            //
-            // convert into
-            //
-            //      track -> thin wall -> track
-            //
-            foreach (var position in track.Where (t => t.Value == Area.Track).ToArray ()) {
-                foreach (var adjacent in new[] { Left, Right, Up, Down }) {
-                    if (track.GetValueOrDefault (position.Key + adjacent, Area.ThinWall) == Area.Wall &&
-                        track.GetValueOrDefault (position.Key + adjacent + adjacent, Area.ThinWall) == Area.Track) {
-                        track[position.Key + adjacent] = Area.ThinWall;
-                    }
-                }
-            }
+            // get the track in the correct order
+            var path = SortTrack (trackPoints, start);
 
-            var regularDistance = CalculateDistance (track, start, start, end);
-
-            var thinWalls = track.Where (t => t.Value == Area.ThinWall).ToArray ();
-
-            // Now, shortest distance which each thin wall removed.
-            var distances = thinWalls
-                .Select ((t, i) => CalculateDistance (track, t.Key, start, end))
-                .ToArray ();
-
-            Console.WriteLine ($"Q1: {distances.Count (t => regularDistance - t >= 100)}");
+            // Q1 and Q2 are the same now.
+            Console.WriteLine ($"Q1 {CountCheats (path,  cheatLength: 2, minimumSaving: 100)}");
+            Console.WriteLine ($"Q2 {CountCheats (path, cheatLength: 20, minimumSaving: 100)}");
         }
 
-        static int CalculateDistance (Dictionary<Position, Area> track, Position thinWall, Position start, Position end)
+        static int CountCheats (List<Position> path, int cheatLength, int minimumSaving)
         {
-            var queue = new PriorityQueue<Position, int> ([(start, 0)]);
-            var dist = new Dictionary<Position, int> { { start, 0 } };
+            return path
+                .Pairwise (minimumSaving)
+                .Where (pair => pair.right.item.ManhattenDistance (pair.left.item) <= cheatLength)
+                .Select (pair => pair.right.itemIndex - pair.left.itemIndex - pair.right.item.ManhattenDistance (pair.left.item))
+                .Count (t => t >= minimumSaving);
+        }
 
-            while (queue.Count > 0) {
-                var currentPos = queue.Dequeue ();
-                foreach (var adjacent in new[] { Left, Right, Up, Down }) {
-                    var nextPos = currentPos + adjacent;
-                    if (dist.ContainsKey (nextPos))
-                        continue;
-                    if (track[nextPos] == Area.Track || nextPos == thinWall) {
-                        dist[nextPos] = dist[currentPos] + 1;
-                        queue.Enqueue (nextPos, dist[nextPos]);
+        static List<Position> SortTrack (HashSet<Position> trackPoints, Position start)
+        {
+            var result = new List<Position> ();
+            trackPoints = trackPoints.ToHashSet ();
+            while (trackPoints.Count > 0) {
+                result.Add (start);
+                trackPoints.Remove (start);
+                foreach (var option in Position.AllDirections) {
+                    if (trackPoints.Contains (start + option)) {
+                        start += option;
+                        break;
                     }
                 }
             }
-
-            return dist[end];
+            return result;
         }
     }
 }
